b2add26ed3d1490a42c6688ebcbfd60b5fb1d421
kevin regenrek <hello@regenrek.at>
2020-04-18 09:27:02 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="grid grid-cols-3 gap-4">
+      <div class="col-span-1">
+        <Logo/>
+        <h3>My Blog</h3>
+        <ul>
+          <li>
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">About</nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div class="col-span-2">
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
